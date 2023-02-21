47c691fc3c7f296fdba80a47aa47ff59822081ff
piroboyd <bojdakrystian@gmail.com>
2023-02-21 15:32:16 +0100

    added user interface graphics files, basic window setup

--- tkinter01.py
+++ tkinter01.py
@@ -1,10 +1,11 @@
 from tkinter import *
 
-window = Tk() # Making instance of window
+window = Tk()  # Making instance of window
 window.geometry("420x420")
 window.title("Etykieciarz")
 
 icon = PhotoImage(file="grafika/logo.png")
 window.iconphoto(True, icon)
+window.config(background="#c8c7d1")
 
-window.mainloop() # Place window on computer screen
+window.mainloop()  # Place window on computer screen
